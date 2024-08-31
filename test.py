771ba915dffcee1bd5c4f1b7f88f80ba3bc4fedd
Marcus-Milantoni <119524125+Marcus-Milantoni@users.noreply.github.com>
2024-08-31 13:19:27 -0400

    Delete test.py
    deleted the test before bringing to main

--- test.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-tuple_of_examples = tuple("apple", "banana", "cherry")
-
-tuple_of_what_we_want = tuple("apple", "cherry")
-
-for example in tuple_of_examples:
-    if example is not None:
-        
